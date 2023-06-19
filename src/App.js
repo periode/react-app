b8fa24ac21d3c8037a470256a145b9480b929f81
période <pierre.depaz@gmail.com>
2023-06-19 12:28:34 +0200

    added 3d models

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from './Post.js';
+import Login from './Login.js'
+import Box from './Box.js'
+import Model from './Model.js'
+
+import { MapContainer } from 'react-leaflet/MapContainer'
+import { Marker } from 'react-leaflet/Marker'
+import { Popup } from 'react-leaflet/Popup'
+import { TileLayer } from 'react-leaflet/TileLayer'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>hello!</h1>
+
+        {/* <div id="map">
+          <MapContainer center={[51.605, -0.09]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.605, -0.09]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div> */}
+
+        <div className="canvasContainer">
+          <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <OrbitControls/>
+            <Model/>
+          </Canvas>
+        </div>
+
+        <Login />
+        <Post title="my first one" content="lorem ipsum"></Post>
+        <Post title="the second one" content="lorem ipsum 2"></Post>
         <a
           className="App-link"
           href="https://reactjs.org"
